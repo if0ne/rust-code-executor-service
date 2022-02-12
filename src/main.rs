1cb91412a3ef552f0b159e680f1ca7b8037ddba3
if0nee <pavel.agafonov.al@gmail.com>
2022-02-12 18:10:24 +0300

    program execute example

--- src/main.rs
+++ src/main.rs
@@ -6,15 +6,34 @@ extern crate rocket;
 use crate::mesure::ProcessInformer;
 use rocket::http::Status;
 use rocket::response::status;
+use std::io::Write;
 
 #[get("/")]
-async fn index<'o>() -> status::Custom<String> {
+async fn index() -> status::Custom<String> {
     let mut process = std::process::Command::new("rustc")
+        .arg("-O")
         .arg("test.txt")
+        .arg("--out-dir")
+        .arg("target/debug/")
         .spawn()
         .unwrap();
-    let info = process.get_process_info();
-    status::Custom(Status::Ok, format!("{:?}", info))
+    let compile_info = process.get_process_info();
+
+    //Вывод результата происходит в файл
+    let outputs = std::fs::File::create("out.txt").unwrap();
+    let mut process = std::process::Command::new("test.exe")
+        .stdin(std::process::Stdio::piped())
+        .stdout(std::process::Stdio::from(outputs))
+        .spawn()
+        .unwrap();
+    //Ввод данных в процесс
+    process.stdin.as_mut().unwrap().write_all(b"Pavel").unwrap();
+    let program_info = process.get_process_info();
+
+    status::Custom(
+        Status::Ok,
+        format!("{:?}\n{:?}", compile_info, program_info),
+    )
 }
 
 #[rocket::main]
