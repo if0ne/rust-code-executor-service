df0f8126584cd4bf7b1e94c3ab919f3030c1b29f
if0nee <pavel.agafonov.al@gmail.com>
2022-02-28 19:56:10 +0300

    fix time mesure

--- src/mesure/win_info.rs
+++ src/mesure/win_info.rs
@@ -10,34 +10,9 @@ use std::time::Duration;
 impl ProcessInformer for std::process::Child {
     async fn get_process_info(&mut self) -> Result<ProcessInfo, Box<dyn Error>> {
         let instance = self.as_raw_handle() as isize;
+        let start = std::time::Instant::now();
         let _status = self.wait()?;
-
-        let (user_time, _) = unsafe {
-            let mut ctime = mem::zeroed();
-            let mut etime = mem::zeroed();
-            let mut kernel_time = mem::zeroed();
-            let mut user_time = mem::zeroed();
-            let res = windows::Win32::System::Threading::GetProcessTimes(
-                windows::Win32::Foundation::HANDLE(instance),
-                &mut ctime,
-                &mut etime,
-                &mut kernel_time,
-                &mut user_time,
-            );
-
-            if res.as_bool() {
-                //Умножение на 100 для перевода в наносекунды, т.к. функция возвращает время измеряемой в 100 наносекундах
-                let user = (((user_time.dwHighDateTime as i64) << 32)
-                    + user_time.dwLowDateTime as i64)
-                    * 100;
-                let kernel = (((kernel_time.dwHighDateTime as i64) << 32)
-                    + kernel_time.dwLowDateTime as i64)
-                    * 100;
-                (user as u64, kernel as u64)
-            } else {
-                (0, 0)
-            }
-        };
+        let delta = start.elapsed();
 
         let total_bytes = unsafe {
             let mut pmc = mem::zeroed();
@@ -55,7 +30,7 @@ impl ProcessInformer for std::process::Child {
         };
 
         Ok(ProcessInfo {
-            execute_time: Duration::from_nanos(user_time),
+            execute_time: delta,
             total_memory: total_bytes,
         })
     }
